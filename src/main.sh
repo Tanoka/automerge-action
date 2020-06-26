dd6bfd9d90df66b63b98909b78b09473326c7ff0
Tanoka <david.urreta@gmail.com>
2020-06-26 15:06:44 +0200

    Update main.sh

--- src/main.sh
+++ src/main.sh
@@ -13,19 +13,22 @@ main() {
   export GITHUB_TOKEN="$1"
  echo "INIII"
  local -r commit_sha="$(github_actions::commit_sha)"
- echo "$commit_sha"
+ echo "sha $commit_sha"
  local -r commited_files=$(github::get_commit_modified_files "$commit_sha")
  
  local -r num_files=$(echo "$commited_files" | wc -l)
- echo "$num_files"
+ echo "num $num_files"
  if [ $num_files -eq 1 ]; then
-    echo "$commited_files"
+    echo "name $commited_files"
     if [ "$commited_files" == "composer.lock" ]; then
       echo "INNNN"
       local -r pr_number="$(github_actions::get_pr_number)"
       github::merge_pull_request "$pr_number"
+      echo "IF 2"
     fi
+    echo "IF 1"
  fi
+ echo "FIN"
 
   exit $?
 }
