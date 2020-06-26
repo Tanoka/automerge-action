a892307cc9c8d2f74018a1fe91adb632926340b1
Tanoka <david.urreta@gmail.com>
2020-06-26 15:43:52 +0200

    Update github_actions.sh

--- src/github_actions.sh
+++ src/github_actions.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 github_actions::get_pr_number() {
-  jq --raw-output .pull_request.number "$GITHUB_EVENT_PATH"
+  jq --raw-output .number "$GITHUB_EVENT_PATH"
 }
 
 github_actions::commit_sha() {
